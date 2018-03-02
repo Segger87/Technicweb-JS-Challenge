4df75479ff931684d25dc80530e7c63cca5f4c6e
Sam Egger <segger87@gmail.com>
2018-03-02 19:25:03 +0000

    update bug fixes, speed up the blocks

--- JS-technicweb/js/script.js
+++ JS-technicweb/js/script.js
@@ -1,8 +1,12 @@
-const squareX = document.getElementById('square-x');
-const squareY = document.getElementById('square-y');
-const container = document.getElementById('container');
-const updateScoreX = document.getElementById('score-x');
-const updateScoreY = document.getElementById('score-y');
+const squareX = document.getElementById("square-x");
+const squareY = document.getElementById("square-y");
+const gameContainer = document.getElementById("container");
+const updateScoreX = document.getElementById("score-x");
+const updateScoreY = document.getElementById("score-y");
+const screenLength = screen.width;
+const halfLength = Math.floor(screenLength / 2);
+console.log(screenLength);
+console.log(halfLength);
 
 //speed variables
 let squareSpeedX = 0;
@@ -11,7 +15,7 @@ let squareSpeedY = 0;
 let squareSpeedYrand = randNum();
 //score variables
 let xScore = 0;
-let yScore = 0
+let yScore = 0;
 //variables to trigger horizontal if statements
 let squareLeftCount = 0;
 let squareRightCount = 0;
@@ -23,52 +27,58 @@ let squareScoreCountX = 0;
 let squareScoreCountY = 0;
 
 //generates a random number on document load
-function randNum(){
+function randNum() {
   return Math.floor(Math.random() * 2) + 1;
 }
 window.onload = randNum();
 
 //used setInterval to execute the move function every 100ms
-const intervalID = window.setInterval(move, 100);
+const intervalID = window.setInterval(move, 25);
 
-function move(){
+function move() {
   //if statements for horizontal square
-  if(squareRightCount === 0){
+  if (squareRightCount === 0) {
     squareSpeedX += squareSpeedXrand; //adds random number speed to the objects speed variable
     squareScoreCountX = squareSpeedX; //updates variable with speed value - this is used to track the score
-    squareX.style.left = squareSpeedX + '%'; //updates the position value of the square
+    squareX.style.left = squareSpeedX + "%"; //updates the position value of the square
     squareLeftCount = squareSpeedX; // updates variable that is used to trigger the square in reverse
-  } if(squareLeftCount === 100){
+  }
+  if (squareLeftCount === 98) {
     squareSpeedX -= squareSpeedXrand;
     squareScoreCountX = squareSpeedX;
-    squareX.style.left= squareSpeedX + '%';
+    squareX.style.left = squareSpeedX + "%";
     squareRightCount = squareSpeedX;
-  } if(squareScoreCountX == 50){
+  }
+  if (squareScoreCountX == 50) {
     xScore += 1;
-    updateScoreX.innerHTML = 'Horizontal Score ' + xScore;
-    squareX.style.backgroundColor = 'green';
-  } if(squareScoreCountX < 49 || squareScoreCountX > 51){
-    squareX.style.backgroundColor = 'blue';
+    updateScoreX.innerHTML = "Horizontal Score " + xScore;
+    squareX.style.backgroundColor = "green";
+  }
+  if (squareScoreCountX < 49 || squareScoreCountX > 51) {
+    squareX.style.backgroundColor = "blue";
   }
   //if statments for vertical square
-   if(squareBottomCount === 0){
-      squareSpeedY += squareSpeedYrand;
-      squareScoreCountY = squareSpeedY;
-      squareY.style.top = squareSpeedY + '%';
-      squareTopCount = squareSpeedY;
-    } if(squareTopCount === 100){
-      squareSpeedY -= squareSpeedYrand;
-      squareScoreCountY = squareSpeedY;
-      squareY.style.top= squareSpeedY + '%';
-      squareBottomCount = squareSpeedY;
-    } if(squareScoreCountY == 50){
-      yScore += 1;
-      updateScoreY.innerHTML = 'Vertical Score ' + yScore;
-      squareY.style.backgroundColor = 'orange';
-    } if(squareScoreCountY < 49 || squareScoreCountY > 51){
-      squareY.style.backgroundColor = 'red';
-    }
+  if (squareBottomCount === 0) {
+    squareSpeedY += squareSpeedYrand;
+    squareScoreCountY = squareSpeedY;
+    squareY.style.top = squareSpeedY + "px";
+    squareTopCount = squareSpeedY;
+  }
+  if (squareTopCount === 558) {
+    squareSpeedY -= squareSpeedYrand;
+    squareScoreCountY = squareSpeedY;
+    squareY.style.top = squareSpeedY + "px";
+    squareBottomCount = squareSpeedY;
+  }
+  if (squareScoreCountY == 300) {
+    yScore += 1;
+    updateScoreY.innerHTML = "Vertical Score " + yScore;
+    squareY.style.backgroundColor = "orange";
+  }
+  if (squareScoreCountY < 298 || squareScoreCountY > 302) {
+    squareY.style.backgroundColor = "red";
+  }
 }
 
-  updateScoreX.innerHTML = 'Horizontal Score ' + xScore;
-  updateScoreY.innerHTML = 'Vertical Score ' + yScore;
+updateScoreX.innerHTML = "Horizontal Score " + xScore;
+updateScoreY.innerHTML = "Vertical Score " + yScore;
